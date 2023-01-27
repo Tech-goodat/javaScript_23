98b8a5f5fd15a5688475daee879622226e4e5e2b
Tech-goodat <felixkiprotich2000@gmail.com>
2023-01-27 11:32:53 +0300

    a function that converts an array to a string

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//adding array elements using the push() method
-const fruits=["Bananas", "Oranges", "Tomatoes"];
-fruits.push("Mangoes");
-
+//converting arrays to strings
+const fruits=["Mangoes", "oranges", "Tomatoes"];
+document.getElementById("demo").innerHTML=fruits.toString () +" "+"are fruits but a potatoe is not a fruit";
