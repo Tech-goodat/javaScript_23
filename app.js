895c7a6516ad46ac3cf91dc372795931557690d9
Tech-goodat <felixkiprotich2000@gmail.com>
2023-01-27 10:51:35 +0300

    a function to loop through an array

---
+++ app.js
@@ -0,0 +1,9 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let text = "<ul>";
+fruits.forEach(myFunction);
+text += "</ul>";
+
+function myFunction(value) {
+  text += "<li>" + value + "</li>";
+}
